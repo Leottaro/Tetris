de2cb3ba828eefdabb229c88610ca4654daa1357
Leottaro <leo.hafdane@gmail.com>
2023-09-27 12:54:24 +0200

    improved piece randomization

--- src/TetroType.java
+++ src/TetroType.java
@@ -3,9 +3,27 @@ import java.util.Random;
 public enum TetroType {
     I, J, L, O, S, T, Z;
 
-    public static TetroType getRandom() {
+    private static int tetroSize = values().length;
+    private static TetroType[] tetroList = new TetroType[tetroSize];
+    private static boolean[] tetroInList = new boolean[tetroSize];
+    private static int tetroI = Integer.MAX_VALUE;
+
+    private static void genList() {
+        tetroInList = new boolean[tetroSize];
         Random rnd = new Random();
-        int i = rnd.nextInt(values().length);
-        return values()[i];
+        for (int i = 0; i < tetroSize; i++) {
+            int choosen = rnd.nextInt(tetroSize);
+            while (tetroInList[choosen])
+                choosen = rnd.nextInt(tetroSize);
+            tetroList[i] = values()[choosen];
+            tetroInList[choosen] = true;
+        }
+        tetroI = 0;
+    }
+
+    public static TetroType getRandom() {
+        if (tetroI >= tetroSize) genList();
+        tetroI++;
+        return tetroList[tetroI-1];
     }
 }
